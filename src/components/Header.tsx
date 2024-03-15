26992769cf39fb625a64f5697f2db57df6253e57
gudnuf <gudnuf21@proton.me>
2024-03-15 11:48:19 -0700

    improve profile in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,95 @@
-import Image from "next/image";
-import { Button, Navbar } from "flowbite-react";
-import ProfileButton from "./ProfileButton";
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NDKNip07Signer, NDKUserProfile } from "@nostr-dev-kit/ndk";
+import { useNdk } from "@/hooks/useNdk";
+import { RootState } from "@/redux/store";
+import { setUser } from "@/redux/slices/UserSlice";
+import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [profile, setProfile] = useState<NDKUserProfile>();
+
+  const dipsatch = useDispatch();
+
+  const { setSigner, removeSigner, fetchUserProfile } = useNdk();
+
+  const user = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    if (user.pubkey) {
+      setLoggedIn(true);
+      fetchUserProfile(user.pubkey).then(setProfile);
+    } else {
+      setLoggedIn(false);
+    }
+  }, [user, fetchUserProfile]);
+
+  useEffect(() => {
+    const pubkey = window.localStorage.getItem("pubkey");
+    if (pubkey) {
+      dipsatch(setUser({ pubkey: pubkey }));
+      setSigner(new NDKNip07Signer())
+    }
+  }, []);
+
+  const handleLogout = () => {
+    dipsatch(setUser({ pubkey: "" }));
+    window.localStorage.removeItem("pubkey");
+    removeSigner();
+  };
+
+  const handleLogin = async () => {
+    if (!window.nostr) {
+      alert("Nip07 extension not found");
+      return;
+    }
+    try {
+      const pubkey = await window.nostr.getPublicKey();
+      if (pubkey) {
+        dipsatch(setUser({ pubkey }));
+        window.localStorage.setItem("pubkey", pubkey);
+      } else {
+        throw new Error("No pubkey");
+      }
+      setSigner(new NDKNip07Signer());
+    } catch (e) {
+      console.error(e);
+      alert("Could not find nip07 extension");
+    }
+  };
   return (
-    <Navbar fluid rounded className="bg-gray-800">
-      <Navbar.Brand>
-        <Image src="/bitcoinmintslogo.png" alt="" width={350} height={350} className="mr-3"/>
+    <Navbar
+      fluid
+      rounded
+      className="bg-gray-800 flex justify-around align-middle items-center"
+    >
+      <Navbar.Brand className="md:ml-6 w-2/3 md:w-1/3">
+        <img src="/bitcoinmintslogo.png" alt="" />
       </Navbar.Brand>
-      <div>
-        <ProfileButton />
-        {/* <Navbar.Toggle /> */}
+      <div className="flex justify-end md:order-2 md:mr-6">
+        {loggedIn ? (
+        <Dropdown arrowIcon={false} inline label={ <Avatar
+          img={profile?.image}
+          alt="Profile Picture"
+          className="mr-3"
+         />}>
+          <Dropdown.Header>
+            <span className="block truncate text-sm font-medium">
+              {profile?.name || profile?.displayName || ""}
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
+        </Dropdown>
+        ) : (
+          <Button color="light" pill onClick={handleLogin}>
+            Login
+          </Button>
+        )}
       </div>
-      {/* <Navbar.Collapse> */}
-        {/* <Navbar.Nav>
-          <Navbar.Item active>Home</Navbar.Item>
-          <Navbar.Item>Features</Navbar.Item>
-          <Navbar.Item>Services</Navbar.Item>
-          <Navbar.Item>About</Navbar.Item>
-          <Navbar.Item>Contact</Navbar.Item>
-        </Navbar.Nav>
-      </Navbar.Collapse> */}
     </Navbar>
-  )
+  );
 };
 
 export default Header;
