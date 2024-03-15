f056be3e5f6be22f651a8672de11405468d57b3d
gudnuf <gudnuf21@proton.me>
2024-03-15 10:53:37 -0700

    include last digits in shortened url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export const shortenString = (str: string) => {
   if (str.length < 25) return str;
-  return str.slice(0, 25) + "...";
+  return str.slice(0, 20) + "..." + str.slice(-4);
 }
 
 export const copyToClipboard = (str: string) => {
