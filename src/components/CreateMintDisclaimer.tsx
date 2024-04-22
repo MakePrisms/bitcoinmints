829cc603554a1c2c2b8ca320ceabd997fd3bfc5a
gudnuf <gudnuf21@proton.me>
2024-04-22 09:38:54 -0700

    update disclaimer text

--- src/components/CreateMintDisclaimer.tsx
+++ src/components/CreateMintDisclaimer.tsx
@@ -12,8 +12,31 @@ const CreateMintDisclaimer = () => {
         <h2 className="text-2xl">Warning!</h2>
       </div>
       <Modal.Body>
+        <p className="mb-3">
+          Create Mint is in early BETA! We hold no responsibility for people
+          losing access to funds. Use at your own risk!
+        </p>
+        <p className="mb-3">
+          Create Mint is an experimental feature based on the Cashu and Nostr
+          protocols which are still extremely early in development. This
+          functionality will certainly change in the near future and mints will
+          no longer work. Do not plan to use a mint for an extended period of
+          time.
+        </p>
+        <p className="mb-3">
+          Create Mint is non-custodial software - this software does not custody
+          any bitcoin or any ecash tokens. This means if you lose access to your
+          bitcoin or ecash tokens stored on your wallet, there is no way to
+          recover them using our software.
+        </p>
         <p>
-          Insert disclaimer text here. This is a disclaimer for creating a mint.
+          Terms of service can be found at{" "}
+          <a
+            className="text-cyan-400 underline"
+            href="https://www.makeprisms.com/terms"
+          >
+            https://www.makeprisms.com/terms
+          </a>
         </p>
       </Modal.Body>
       <Modal.Footer className="flex justify-between">
